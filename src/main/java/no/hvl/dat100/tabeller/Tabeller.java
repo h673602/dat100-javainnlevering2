11395a3b9c0c1b7f6eef3ca9246106ec460042cc
h673602 <673602@stud.hvl.no>
2024-10-04 20:56:55 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,38 +5,56 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		for (int i = 0; i < tabell.length; i++) {
 
+			System.out.println(tabell[i]);
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		String[] tall = {};
+
+		tall[0] = "42";	
+		tall[1] = "67";
+		tall[2] = "89";
+		
+		return tall;
+		
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+
+		for (int i = 0; i < tabell.length; i++) {
+
+			sum += tabell[i];
+		}
+		return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
+		for (int i : tabell) {
+	        if (i == tall) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		for (int i : tabell) {
+	        if (i == tall) {
+	            return i;
+	        }
+	    }
+	    return -1;
 	}
 
 	// f)
